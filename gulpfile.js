c8e8b114233829a7775e1eb9dadd90bc53f83e74
kevin <kwagner1312@gmail.com>
2020-07-18 07:53:32 -0500

    add gulp-imagemin task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('sass', function() {
     return gulp.src('scss/gulp-test.scss')
     .pipe(sass()) //using gulp-sass
     .pipe(gulp.dest('dist/css/'))
 });
+
+gulp.task('imgSquash', imgSquash);
+
+function imgSquash() {
+    return gulp
+    .src("img/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/img/"));
+}
